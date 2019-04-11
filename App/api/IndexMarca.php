70bd9bf59d7644cd35df8c4b79e7fb940c044dc9
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2019-04-11 13:15:30 -0500

    dev routes

--- App/api/IndexMarca.php
+++ App/api/IndexMarca.php
@@ -2,6 +2,15 @@
 // require_once '../../autoload.php';
 require __DIR__.'/../../bootstrap/autoload.php';
 
+$router = new AltoRouter();
+
+// map homepage
+$router->map( 'GET', '/', function() {
+    require __DIR__ . '/views/home.php';
+});
+
+return ;
+
 use App\Controllers\MarcaController ;
 
 $suscrito_controller = new MarcaController() ;
