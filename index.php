5184d588f0ab8674b087e4359d55830aa6077189
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2022-03-28 23:14:25 -0500

    estructurando

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-// require_once '../../autoload.php';
-require __DIR__.'/bootstrap/autoload.php';
+// // require_once '../../autoload.php';
+// require __DIR__.'/bootstrap/autoload.php';
 
-// $router->setBasePath('/orm-eloquent-php/public/');
+// // $router->setBasePath('/orm-eloquent-php/public/');
 
-// var_dump($router);
+// // var_dump($router);
 
-// map homepage
-$router->map( 'GET', '/', function() {
-    require __DIR__ . '/app/views/mail.html';
-    // echo "prueba demo";
-});
+// // map homepage
+// $router->map( 'GET', '/', function() {
+//     require __DIR__ . '/app/views/mail.html';
+//     // echo "prueba demo";
+// });
 
-return ;
+// return ;
+
+header("Location: ./src/");
